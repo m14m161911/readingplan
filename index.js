654557c296b5d5b07a1db129222d08582ea7fbfd
Jon Van Slyke <m14m161911@gmail.com>
2020-05-12 08:53:07 -0700

    added external link for button 7

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ let content = [
     Interdependent relationships in the Body of Christ in advancing the Gospel. Phil 1:3-14 <br>`,
     `We see a vital movement of the Gospel, fueled by prevailing prayer, flowing freely through relational networks and
     out into the nations. Workers for the Kingdom are next door to everywhere!  What characterizes this movement? A heart for the whole person ... climates of grace ... compassion for the vulnerable and broken ... sacrificial unity embracing diversity ... cultural relevance and sensitivity ... interdependence with others in the wider family of God ... transformed men and women, fragrant with humility and the aroma of Christ. They are marked by a deep engagement with and obedience to the Scriptures as the Word of God. They believe the promises of God. Both personally and in committed communities, they seek to know and pursue the purposes of God. Laborers and leaders are emerging, with an increasing passion for Jesus Christ. They demonstrate faith and courage as they live and move among their friends and families. As spiritual parents, they model authenticity and relevance. Ordinary people, in many walks of life, are joyfully leading integrated lives. They live as fruitful insiders among the lost. There is perseverance in the face of hardship and suffering. Around the world, many are coming to faith. As they become   established in discipleship, some grow to be foundational for further generations. The Gospel spreads naturally and powerfully, as believers share Christ ... life upon life ... family to family. Crossing cultures into new cities and nations, teams of mobile pioneers intentionally proclaim and embody the good news of Jesus Christ, in such a way that transformed communities multiply. These communities are bringing joy and hope to their surrounding environments as relationships are healed and justice increases. Indeed, the lost and unreached burn in their hearts, as they move the Gospel into the nations. The leaders of this movement, developed and empowered for God’s service, live out a growing commitment to Christlikeness. They are dependent upon the Holy Spirit. New generations of leaders are emerging, rooted incarnationally in their local and national contexts. An international leadership community brings focus, alignment and energy to their movement. These leaders are clearly committed to long-term impact in 
-    generational ministry.`
+    generational ministry.`,
+    `Redirected to an external web page.`
 ]
 console.log(content);
 
@@ -127,6 +128,14 @@ window.onload = function(){
         main.innerHTML = content[6];
     })
     button7.addEventListener('click', function (){
-        
+        main.innerHTML = content[7];
+        var win = window.open('https://www.navigators.org/staff/23831807/', '_blank');
+        if (win) {
+            //Browser has allowed it to be opened
+            win.focus();
+        } else {
+            //Browser has blocked it
+            alert('Please allow popups for this website');
+        }
     })
 }
